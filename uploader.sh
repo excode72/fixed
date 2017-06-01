18d149803789733e9abdd2fb19cf9951f2db1f63
excode72 <nevadarouter@gmail.com>
2017-06-01 07:19:28 -0500

    Update uploader.sh

--- uploader.sh
+++ uploader.sh
@@ -2,12 +2,13 @@ echo -e "Welcome To File uploader" | lolcat
 echo -e "Script by ExCode72, Powered by Transfer.sh" | lolcat
 echo ""
 echo "===============================================================" | lolcat
-echo =e "1.Isikan File Path dengan lokasi file anda. (Contoh: /home/babi)" | lolcat
-echo -e "   2.Isikan Nama File sesuai nama file anda. (Contoh: babi)" | lolcat
+echo =e "1.Isikan File Path dengan lokasi file anda. (Contoh: /home/babi.txt)" | lolcat
+echo -e "   2.Isikan Nama File sesuai nama file anda. (Contoh: babi.txt)" | lolcat
 echo "===============================================================" | lolcat
+read -p "File Path: " pathanda
+read -p "Nama File: " fileanda
 
-read -p "File Path: " path1
-read -p "Nama File (dengan format): " nama1
 echo -e "Berikut Link URL anda :"
-curl --upload-file $path1 https://transfer.sh/$nama1
+curl --upload-file $pathanda https://transfer.sh/$fileanda
+echo -e ""
 echo -e "Your File Has been Successfully Uploaded" | lolcat
