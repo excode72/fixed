e34c918b4c306610ea962d196103486744c1d78b
excode72 <nevadarouter@gmail.com>
2017-06-01 07:20:53 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -186,22 +186,7 @@ do
 	 ;;
 	 "File Uploader (NEW!)")
 	 clear
-	 echo -e "Welcome To File uploader" | lolcat
-echo -e "Script by ExCode72, Powered by Transfer.sh" | lolcat
-echo ""
-echo ""
-echo "===============================================================" | lolcat
-echo =e "1.Isikan File Path dengan lokasi file anda. Contoh: /home/babi.sh" | lolcat
-echo -e "   2.Isikan Nama File sesuai nama file anda. Contoh: babi.sh | lolcat
-echo "===============================================================" | lolcat
-
-read -p "File Path: " path1
-read -p "Nama File: " nama1
-echo -e "Berikut Link URL anda :"
-curl --upload-file $path1 https://transfer.sh/$nama1
-echo ""
-echo -e "Your File Has been Successfully Uploaded" | lolcat
-
+	 uploader | lolcat
 break
 ;;
 
