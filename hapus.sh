68929fa7bd3854071221bd930e0bbd43dc9846f8
excode72 <nevadarouter@gmail.com>
2017-06-02 01:12:19 -0500

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Script Hapus User SSH
+userlist
+echo -e ""
 read -p "Username yang akan dihapus : " Login
 userdel $Login
 echo -e ".................................................."
+echo -e ""
 echo -e "User \e[1;33;44m$Login\e[0m Berhasil dihapus"
