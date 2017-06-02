f4bfba8defc8aa9829ec672c8af3f3902843485b
excode72 <nevadarouter@gmail.com>
2017-06-02 01:03:34 -0500

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #Script auto create trial user SSH
-#yg akan expired setelah 1 hari
-#www.fawzya.net - 085799054816
 Login=trial-`</dev/urandom tr -dc X-Z0-9 | head -c4`
-masaaktif="1"
+expire="1"
 Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
-IP=`dig +short myip.opendns.com @resolver1.opendns.com`
-useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+IP=`wget -qO- ipv4.icanhazip.com`
+useradd -e `date -d "$expire days" +"%Y-%m-%d"` -s /bin/false -M $Login
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e "Akun Trial anda sudah jadi......."
+echo -e "==================================="
 echo -e "Host: $IP" 
-echo -e "Port: 443,143,80"
-echo -e "Squid Port: 8080"
+echo -e "Port: 443"
+echo -e "Squid Port: 8080,80"
 echo -e "Username: $Login "
 echo -e "Password: $Pass\n"
+echo -e "==================================="
 echo -e ""
 echo -e "Akun ini hanya aktif 1 hari"
-echo -e "Script by ExCode72
-echo -e ""
+echo -e "Script by ExCode72"
