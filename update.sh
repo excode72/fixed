75eede786be0af11662580db3b9bc95ef7f1a26d
excode72 <nevadarouter@gmail.com>
2017-06-07 08:36:03 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,10 @@
 cd
-wget -O trial "https://raw.githubusercontent.com/excode72/fixed/centos8/trial.sh"
-wget -O hapus "https://raw.githubusercontent.com/excode72/fixed/centos8/hapus.sh"
-mv trial /usr/bin
-mv hapus /usr/bin
-chmod +x /usr/bin/trial
-chmod +x /usr/bin/hapus
+wget -O listharga "https://raw.githubusercontent.com/excode72/fixed/centos8/listharga.sh"
+mv listharga /usr/bin
+chmod +x /usr/bin/listharga
 echo -e "Update kali ini :" | lolcat
-echo -e "Script Trial................!" | lolcat
-echo -e "Script Hapus User...........!" | lolcat
+echo -e "Script List Harga................!" | lolcat
+echo -e "Script Edit List Harga...........!" | lolcat
 echo -e ""
 echo -e "Semua Script diatas berhasil di Update!!" | lolcat
 rm update.sh 
