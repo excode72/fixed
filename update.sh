8497714065e397adf2c0eb66245faac4c8740a53
excode72 <nevadarouter@gmail.com>
2018-05-13 13:51:10 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,9 @@
 cd
-wget -O listharga "https://raw.githubusercontent.com/excode72/fixed/centos8/listharga.sh"
-mv listharga /usr/bin
-chmod +x /usr/bin/listharga
+wget -O uploader "https://raw.githubusercontent.com/excode72/fixed/centos8/uploader.sh"
+mv uploader /usr/bin
+chmod +x /usr/bin/uploader
 echo -e "Update kali ini :" | lolcat
-echo -e "Script List Harga................!" | lolcat
-echo -e "Script Edit List Harga...........!" | lolcat
+echo -e "Script Uploader................!" | lolcat
 echo -e ""
 echo -e "Semua Script diatas berhasil di Update!!" | lolcat
 rm update.sh 
